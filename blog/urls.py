914822265441e2b2ff24b6bd5ab74754c8f5e735
Aykut Ulış <aykutulis@gmail.com>
2023-04-07 02:40:16 +0300

    feat: layout and main page

--- blog/urls.py
+++ blog/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from article.views import index
+from article import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', views.index, name='index'),
 ]
