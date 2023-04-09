d5c544a91f10e419f4c22f513fcb78db7695d6d3
Aykut Ulış <aykutulis@gmail.com>
2023-04-10 02:38:19 +0300

    feat: article search functionality

--- article/views.py
+++ article/views.py
@@ -72,6 +72,6 @@ def articles(request):
     keyword = request.GET.get('keyword')
     if keyword:
         articles = Article.objects.filter(title__contains=keyword)
-        return render(request, 'articles.html', {'articles': articles})
+        return render(request, 'articles.html', {'articles': articles, 'keyword': keyword})
     articles = Article.objects.all()
     return render(request, 'articles.html', {'articles': articles})
